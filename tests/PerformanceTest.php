9cbbd0b29066aa75a4511dca85a07e6bac554bb0
SmetDenis <denis@jbzoo.com>
2016-03-24 20:57:44 +0600

    fix type in header new phpunit version and config

--- tests/PerformanceTest.php
+++ tests/PerformanceTest.php
@@ -2,7 +2,7 @@
 /**
  * JBZoo __PACKAGE__
  *
- * This file is part of tPerformanceTesthe JBZoo CCK package.
+ * This file is part of the JBZoo CCK package.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
